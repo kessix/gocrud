58b056773defc178f7f7ff4405d58252c343aa77
kessix <kessis01@gmail.com>
2024-08-31 00:42:36 -0300

    feat: integrate routes and controllers

---
+++ src/controller/createUser.go
@@ -0,0 +1,12 @@
+package controller
+
+import (
+	"github.com/kessix/gocrud/src/configuration/rest_err"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	err := rest_err.NewBadRequestError("Voce chamou a rota de forma errada")
+	c.JSON(err.Code, err)
+}
